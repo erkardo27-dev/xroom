5e67e2bb550e75c9410a43e6a89366c4d3fd23c6
erka erka <erkardo27@gmail.com>
2025-11-16 09:31:40 +0000

    шүүлтүүрийн слайдеруудын бөөрөнхий айконууд алга болсон байх юм

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -6,6 +6,20 @@ import * as SliderPrimitive from "@radix-ui/react-slider"
 
 import { cn } from "@/lib/utils"
 
+const SliderThumb = React.forwardRef<
+  React.ElementRef<typeof SliderPrimitive.Thumb>,
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Thumb>
+>((props, ref) => (
+  <SliderPrimitive.Thumb
+    ref={ref}
+    className={cn(
+      "block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+      props.className
+    )}
+  />
+));
+SliderThumb.displayName = SliderPrimitive.Thumb.displayName;
+
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
@@ -21,24 +35,11 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
       <SliderPrimitive.Range className="absolute h-full bg-primary" />
     </SliderPrimitive.Track>
-    {props.children}
+    {props.children ?? <SliderThumb />}
   </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
 
-const SliderThumb = React.forwardRef<
-  React.ElementRef<typeof SliderPrimitive.Thumb>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Thumb>
->((props, ref) => (
-  <SliderPrimitive.Thumb
-    ref={ref}
-    className={cn(
-      "block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
-      props.className
-    )}
-  />
-));
-SliderThumb.displayName = SliderPrimitive.Thumb.displayName;
 
 Slider.Thumb = SliderThumb;
 
