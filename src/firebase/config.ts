cc958f2a227cacebd56d41106c88b3a7efbd9523
erka erka <erkardo27@gmail.com>
2025-11-23 18:17:24 +0000

    uur zunduu huvisagchid baih yostoi shdee oruulaach

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "xroom-backup-36212476-3f972",
-  "appId": "1:877331471792:web:fa413fec24711849558a75",
-  "apiKey": "AIzaSyBQ-110dxJFzeBgxAoD-mfoZCbPafuMNTY",
-  "authDomain": "xroom-backup-36212476-3f972.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "877331471792"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
