9dd871e9386c946b5a88ac538f6d5b425da52bcd
erka erka <erkardo27@gmail.com>
2025-11-23 17:38:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "xroom-backup-36212476-3f972",
+  "appId": "1:877331471792:web:fa413fec24711849558a75",
+  "apiKey": "AIzaSyBQ-110dxJFzeBgxAoD-mfoZCbPafuMNTY",
+  "authDomain": "xroom-backup-36212476-3f972.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "877331471792"
+};
