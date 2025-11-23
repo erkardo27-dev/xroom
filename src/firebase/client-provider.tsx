e7632371b51140884c08eb786da07d339b8bf7f0
erka erka <erkardo27@gmail.com>
2025-11-23 18:26:27 +0000

    aldaa heveeree bn

--- src/firebase/client-provider.tsx
+++ src/firebase/client-provider.tsx
@@ -3,7 +3,12 @@
 
 import React, { useMemo, type ReactNode } from 'react';
 import { FirebaseProvider } from '@/firebase/provider';
-import { initializeFirebase } from '@/firebase';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+import { firebaseConfig } from './config';
+
 
 interface FirebaseClientProviderProps {
   children: ReactNode;
@@ -12,7 +17,11 @@ interface FirebaseClientProviderProps {
 export function FirebaseClientProvider({ children }: FirebaseClientProviderProps) {
   const firebaseServices = useMemo(() => {
     // Initialize Firebase on the client side, once per component mount.
-    return initializeFirebase();
+    const app = initializeApp(firebaseConfig);
+    const auth = getAuth(app);
+    const firestore = getFirestore(app);
+    const storage = getStorage(app);
+    return { firebaseApp: app, auth, firestore, storage };
   }, []); // Empty dependency array ensures this runs only once on mount
 
   return (
